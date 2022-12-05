0ebc7c448283bf97a3889404a4d96e3d12ac569b
andreavallebona <115356293+andreavallebona@users.noreply.github.com>
2022-12-05 18:16:18 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
-
+#include "lib.h"
+using namespace std;
+int main() {
+    char n=0; cin >> n;
+    if(funzione(n)==true) {
+        cout << n << endl;
+    }else{
+        cout << "errore" << endl;
+    }
+    return 0;
+}
 int main(){
   return 0;
 }
